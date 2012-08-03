340113d6f8d82854c91253d93185487f8981e211
billylieurance <billylieurance@gmail.com>
2012-08-03 06:42:47 +0000

    Removing appid

--- trunk/azure-bing-search-java/src/test/net/billylieurance/azuresearch/test/AzureAppid.java
+++ trunk/azure-bing-search-java/src/test/net/billylieurance/azuresearch/test/AzureAppid.java
@@ -17,6 +17,6 @@ limitations under the License.
 */
 
 public class AzureAppid {
-	public static final String AZURE_APPID = "V+SCgiHOXF9nUrgEhET3f2TaFV7Ba8jQv9CxdybrTmg=";
+	public static final String AZURE_APPID = "";
 	
 }

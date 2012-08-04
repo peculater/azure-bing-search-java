1b8f39ecb89c6a873b0a4822430219edf197c354
William Lieurance <azuresearch@billylieurance.net>
2012-08-04 16:08:59 -0500

    Abstracted Thumbnail because both Video and Image have them.

--- trunk/azure-bing-search-java/src/main/net/billylieurance/azuresearch/AzureSearchImageResult.java
+++ trunk/azure-bing-search-java/src/main/net/billylieurance/azuresearch/AzureSearchImageResult.java
@@ -45,7 +45,7 @@ public class AzureSearchImageResult extends AbstractAzureSearchResult {
 	private Integer _height;
 	private Long _fileSize;
 	private String _contentType;
-	private Thumbnail _thumbnail = new Thumbnail();
+	private BingThumbnail _thumbnail = new BingThumbnail();
 	
 	/**
 	 * @return the mediaUrl
@@ -145,11 +145,11 @@ public class AzureSearchImageResult extends AbstractAzureSearchResult {
 		_contentType = contentType;
 	}
 	
-	public Thumbnail getThumbnail() {
+	public BingThumbnail getThumbnail() {
 		return _thumbnail;
 	}
 
-	public void setThumbnail(Thumbnail thumbnail) {
+	public void setThumbnail(BingThumbnail thumbnail) {
 		_thumbnail = thumbnail;
 	}
 
@@ -157,74 +157,6 @@ public class AzureSearchImageResult extends AbstractAzureSearchResult {
 	
 
 	
-	public class Thumbnail {
-		private String _mediaUrl;
-		private String _contentType;
-		private Integer _width;
-		private Integer _height;
-		private Long _fileSize;
-		/**
-		 * @return the mediaUrl
-		 */
-		public String getMediaUrl() {
-			return _mediaUrl;
-		}
-		/**
-		 * @param mediaUrl the mediaUrl to set
-		 */
-		public void setMediaUrl(String mediaUrl) {
-			_mediaUrl = mediaUrl;
-		}
-		/**
-		 * @return the contentType
-		 */
-		public String getContentType() {
-			return _contentType;
-		}
-		/**
-		 * @param contentType the contentType to set
-		 */
-		public void setContentType(String contentType) {
-			_contentType = contentType;
-		}
-		/**
-		 * @return the width
-		 */
-		public Integer getWidth() {
-			return _width;
-		}
-		/**
-		 * @param width the width to set
-		 */
-		public void setWidth(Integer width) {
-			_width = width;
-		}
-		/**
-		 * @return the height
-		 */
-		public Integer getHeight() {
-			return _height;
-		}
-		/**
-		 * @param height the height to set
-		 */
-		public void setHeight(Integer height) {
-			_height = height;
-		}
-		/**
-		 * @return the fileSize
-		 */
-		public Long getFileSize() {
-			return _fileSize;
-		}
-		/**
-		 * @param fileSize the fileSize to set
-		 */
-		public void setFileSize(Long fileSize) {
-			_fileSize = fileSize;
-		}
-
-	}
 
 
 
---
+++ trunk/azure-bing-search-java/src/main/net/billylieurance/azuresearch/BingThumbnail.java
@@ -0,0 +1,87 @@
+package net.billylieurance.azuresearch;
+
+/*
+ Copyright 2012 William Lieurance
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+public class BingThumbnail {
+	
+		private String _mediaUrl;
+		private String _contentType;
+		private Integer _width;
+		private Integer _height;
+		private Long _fileSize;
+		/**
+		 * @return the mediaUrl
+		 */
+		public String getMediaUrl() {
+			return _mediaUrl;
+		}
+		/**
+		 * @param mediaUrl the mediaUrl to set
+		 */
+		public void setMediaUrl(String mediaUrl) {
+			_mediaUrl = mediaUrl;
+		}
+		/**
+		 * @return the contentType
+		 */
+		public String getContentType() {
+			return _contentType;
+		}
+		/**
+		 * @param contentType the contentType to set
+		 */
+		public void setContentType(String contentType) {
+			_contentType = contentType;
+		}
+		/**
+		 * @return the width
+		 */
+		public Integer getWidth() {
+			return _width;
+		}
+		/**
+		 * @param width the width to set
+		 */
+		public void setWidth(Integer width) {
+			_width = width;
+		}
+		/**
+		 * @return the height
+		 */
+		public Integer getHeight() {
+			return _height;
+		}
+		/**
+		 * @param height the height to set
+		 */
+		public void setHeight(Integer height) {
+			_height = height;
+		}
+		/**
+		 * @return the fileSize
+		 */
+		public Long getFileSize() {
+			return _fileSize;
+		}
+		/**
+		 * @param fileSize the fileSize to set
+		 */
+		public void setFileSize(Long fileSize) {
+			_fileSize = fileSize;
+		}
+
+}

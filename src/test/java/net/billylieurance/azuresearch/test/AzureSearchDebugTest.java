d547cfc2979b5da738d849364abdb0e880cce6be
William Lieurance <azuresearch@billylieurance.net>
2015-06-25 00:14:41 -0500

    Clean up license duplication and unused import

--- src/test/java/net/billylieurance/azuresearch/test/AzureSearchDebugTest.java
+++ src/test/java/net/billylieurance/azuresearch/test/AzureSearchDebugTest.java
@@ -18,7 +18,6 @@ package net.billylieurance.azuresearch.test;
 import java.io.IOException;
 import java.io.InputStream;
 
-import net.billylieurance.azuresearch.AbstractAzureSearchQuery;
 import net.billylieurance.azuresearch.AzureSearchResultSet;
 import net.billylieurance.azuresearch.AzureSearchWebQuery;
 import net.billylieurance.azuresearch.AzureSearchWebResult;
@@ -26,21 +25,6 @@ import net.billylieurance.azuresearch.AzureSearchWebResult;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/*
- Copyright 2012 William Lieurance
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
 public class AzureSearchDebugTest extends AbstractAzureSearchTest {
 
     @Test
